c18249349f2b1476586541f7e0df5b7aae2edee2
Schubster <erwinhrnell@gmail.com>
2023-11-23 10:40:37 +0100

    test

--- kurser.php
+++ kurser.php
@@ -31,11 +31,39 @@
         </nav>
 
         <div class="kurser-grid">
-        <?php
 
+            <div class="kurs kurs-1">
+                <div class="top-color">
+                    <h2>Engelska</h2>
+                </div>
+                <span>Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis cupiditate sit quas quo necessitatibus ea accusantium laudantium quasi consectetur delectus!</span>
+                <a href="">Gå till kurs</a>
+            </div>
 
-        ?>
-            
+            <div class="kurs kurs-2">
+                <h2>Engelska</h2>
+                <span>testestet</span>
+            </div>
+
+            <div class="kurs kurs-3">
+                <h2>Engelska</h2>
+                <span>testestet</span>
+            </div>
+
+            <div class="kurs kurs-4">
+                <h2>Engelska</h2>
+                <span>testestet</span>
+            </div>
+
+            <div class="kurs kurs-5">
+                <h2>Engelska</h2>
+                <span>testestet</span>
+            </div>
+
+            <div class="kurs kurs-6">
+                <h2>Engelska</h2>
+                <span>testestet</span>
+            </div>
 
         </div>
 
