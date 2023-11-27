f5aabf6d1cb9e1cb50035d501d62e421d08ba1f3
apfrog <thecatwithawp@gmail.com>
2023-11-27 14:33:49 +0100

    Update courseview.php

--- Includes/courseview.php
+++ Includes/courseview.php
@@ -12,6 +12,10 @@ if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
 
+/*if(isset($_GET['error'])) {
+    header("location: ../noaccess.php");
+}*/
+
 
 if(!isset($_GET['kursid'])) {
     header("location: ../kurser.php");
@@ -19,21 +23,31 @@ if(!isset($_GET['kursid'])) {
 
 $kursid = (int)$_GET['kursid'];
 
-isUserParticipant();
+//isUserParticipant();
 
-function getCourseColor() {
-    $conn = $GLOBALS['conn']; //accesses conn variable in global scope (1 layer above in this case)
 
-    $kursid = $GLOBALS['kursid'];
 
-    $stmt = $conn->prepare("SELECT *
-                            FROM `webschool`.`course`
-                            WHERE `course_ID`=?;");
-    $stmt->bind_param("i", $kursid);
+function sqlExec($table, $keyword, $value, $type) {
+    $conn = $GLOBALS['conn'];
+
+    $command = "SELECT * FROM `webschool`.`". $table ."` WHERE `". $keyword ."`=?;"; // still ? to prevent sql attacks
+
+    $stmt = $conn->prepare($command); // course_ID = ? for example
+    $stmt->bind_param($type, $value);
 
     $stmt->execute();
+    
+    $value = $stmt->get_result();
+    $stmt = null;
 
-    $result = $stmt->get_result(); // FETCHES RESULT FROM STATEMENT CHECK FOR IF THERE ANY ROWS THAT MATCH TO 
+    return $value;
+}
+
+
+function getCourseColor() {
+    $kursid = $GLOBALS['kursid'];
+
+    $result = sqlExec("course","course_ID",$kursid,"i");
 
     if(mysqli_num_rows($result) == 0) {
         $stmt = null;
@@ -50,18 +64,9 @@ function getCourseColor() {
 }
 
 function getCourseName() {
-    $conn = $GLOBALS['conn']; //accesses conn variable in global scope (1 layer above in this case)
-
     $kursid = $GLOBALS['kursid'];
 
-    $stmt = $conn->prepare("SELECT *
-                            FROM `webschool`.`course`
-                            WHERE `course_ID`=?;");
-    $stmt->bind_param("i", $kursid);
-
-    $stmt->execute();
-
-    $result = $stmt->get_result(); // FETCHES RESULT FROM STATEMENT CHECK FOR IF THERE ANY ROWS THAT MATCH TO 
+    $result = sqlExec("course","course_ID",$kursid,"i");
 
     if(mysqli_num_rows($result) == 0) {
         $stmt = null;
@@ -72,79 +77,52 @@ function getCourseName() {
     else {
         $row = mysqli_fetch_row($result);
         
-
-        $stmt = null;
-
-
-        $stmt = $conn->prepare("SELECT *
-                            FROM `webschool`.`name`
-                            WHERE `name_ID`=?;");
-        $stmt->bind_param("i", $row[1]);
-
-        $stmt->execute();
-
-        $result = $stmt->get_result(); // FETCHES RESULT FROM STATEMENT CHECK FOR IF THERE ANY ROWS THAT MATCH TO 
+        $result = sqlExec("name","name_ID",$row[1],"i");
 
         $row = mysqli_fetch_row($result);
 
         echo($row[1]);
-
-        $stmt = null;
     }
 }
+
+
+
+// EXPERIMENTAL
 
 function isUserParticipant() {
     $userid = "";
 
     $conn = $GLOBALS['conn']; //accesses conn variable in global scope (1 layer above in this case)
 
-    $stmt = $conn->prepare("SELECT *
-                            FROM `webschool`.`users`
-                            WHERE `ssn`=?;");
-    $stmt->bind_param("s", $_SESSION['uid']);
-
-    $stmt->execute();
-
-    $result = $stmt->get_result(); 
+    $result = sqlExec("course","course_ID",$_SESSION['uid'],"i");
 
     if(mysqli_num_rows($result) == 0) {
-    $stmt = null;
-    echo("No rows.");
-    header('Location: ./kursvy.php?error=notfound');
-    //header("location: .");
+        echo("No rows.");
+        header('Location: ./kursvy.php?error=notfound');
+        echo("test");
     }
     else {
         $row = mysqli_fetch_row($result);
         $userid = $row[0];
-
-        $stmt = null;
     }
 
     $stmt = $conn->prepare("SELECT *
                             FROM `webschool`.`course_enrollments`
                             WHERE `course_ID`=? AND `user_ID`=?;");
-
-    $courseid = (int)$_GET["kursid"];
-    $stmt->bind_param("is", $courseid, $userid);
+    $stmt->bind_param("is", $_GET["kursid"], $userid);
 
     $stmt->execute();
-
-    //echo($courseid."/");
-    //echo($userid."/");
     
-
     $result = $stmt->get_result();
 
-    if(mysqli_num_rows($result) == 0) {
+    echo(mysqli_fetch_row($result)[0]);
+
+    /*if(mysqli_num_rows($result) == 0) {
         $stmt = null;
         echo("No rows.");
         header('Location: ./home.php');
     }
-    else {
-        //echo("enrolled");
-
-        $stmt = null;
-    }
+    $stmt = null;*/
 }
 
 function printPost() {
