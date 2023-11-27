f50a8b356c7455398902da227121da5f36c04dd3
grod-ai <61289361+grod-ai@users.noreply.github.com>
2023-11-27 12:35:37 +0100

    updates to kurser

--- kurser.php
+++ kurser.php
@@ -24,17 +24,20 @@
                     </li>
 
                     <div class="left-nav">
-                        <li><a href=""><i class="fa-solid fa-arrow-right-from-bracket"></i> Logga ut</a></li>
+                    <li title="Profil"><a href=""><i class="fa-regular fa-circle-user"></i></a></li>
+                        <li title="Logga ut"><a href=""><i class="fa-solid fa-arrow-right-from-bracket"></i></a></li>
                     </div>
                 </ul>
             </div>
         </nav>
 
+<div class="container">
+
         <nav>
             <div class="vert-nav">
                 <ul>
                     <li><a href=""><i class="fa-solid fa-house"></i> Hem</a></li>
-                    <li><a href=""><i class="fa-solid fa-scroll"></i> Kurser</a></li>
+                    <li><a class="active" href="kurser.php"><i class="fa-solid fa-scroll"></i> Kurser</a></li>
                     <li><a href=""><i class="fa-regular fa-calendar-days"></i> Scheman</a></li>
                     <li><a href=""><i class="fa-solid fa-file-pen"></i> Närvaro</a></li>
                     <li><a href=""><i class="fa-solid fa-newspaper"></i> Nyheter</a></li>
@@ -47,15 +50,19 @@
 
             <div class="kurs kurs-1">
 
-                <h2>Kursnamn</h2>
-                <span>Kurslärare</span>
+                <div class="kurs-top" style="background-color: #16852e;">
+                    <h2>Kursnamn</h2>
+                    <span>Kurslärare</span>
+                </div>
 
                 <div class="kurs-middle">
-                    <i class="fa-regular fa-circle-user"></i>
+                    <div class="circle">
+                   <i class="fa-regular fa-circle-user"></i>
+                   </div>
                 </div>
 
                 <div class="kurs-bottom">
-                    <span>Nästa lektion: 13:30</span>
+                    <span>Nästa lektion: Tis 13:30</span>
                     <span>Klassrum: Sal 11</span>
                 </div>
 
@@ -63,15 +70,19 @@
 
             <div class="kurs kurs-2">
 
-                <h2>Kursnamn</h2>
-                <span>Kurslärare</span>
+            <div class="kurs-top" style="background-color: #1bd52e;">
+                    <h2>Kursnamn</h2>
+                    <span>Kurslärare</span>
+                </div>
 
                 <div class="kurs-middle">
-                    <i class="fa-regular fa-circle-user"></i>
+                    <div class="circle">
+                   <i class="fa-regular fa-circle-user"></i>
+                   </div>
                 </div>
 
                 <div class="kurs-bottom">
-                    <span>Nästa lektion: 13:30</span>
+                    <span>Nästa lektion: Tis 13:30</span>
                     <span>Klassrum: Sal 11</span>
                 </div>
 
@@ -79,15 +90,79 @@
 
             <div class="kurs kurs-3">
 
-                <h2>Kursnamn</h2>
-                <span>Kurslärare</span>
+            <div class="kurs-top" style="background-color: #1bd52e;">
+                    <h2>Kursnamn</h2>
+                    <span>Kurslärare</span>
+                </div>
 
                 <div class="kurs-middle">
-                    <i class="fa-regular fa-circle-user"></i>
+                    <div class="circle">
+                   <i class="fa-regular fa-circle-user"></i>
+                   </div>
                 </div>
 
                 <div class="kurs-bottom">
-                    <span>Nästa lektion: 13:30</span>
+                    <span>Nästa lektion: Tis 13:30</span>
+                    <span>Klassrum: Sal 11</span>
+                </div>
+
+            </div>
+
+            <div class="kurs kurs-4">
+
+            <div class="kurs-top" style="background-color: #1bd52e;">
+                    <h2>Kursnamn</h2>
+                    <span>Kurslärare</span>
+                </div>
+
+                <div class="kurs-middle">
+                    <div class="circle">
+                   <i class="fa-regular fa-circle-user"></i>
+                   </div>
+                </div>
+
+                <div class="kurs-bottom">
+                    <span>Nästa lektion: Tis 13:30</span>
+                    <span>Klassrum: Sal 11</span>
+                </div>
+
+            </div>
+
+            <div class="kurs kurs-5">
+
+            <div class="kurs-top" style="background-color: #1bd52e;">
+                    <h2>Kursnamn</h2>
+                    <span>Kurslärare</span>
+                </div>
+
+                <div class="kurs-middle">
+                    <div class="circle">
+                   <i class="fa-regular fa-circle-user"></i>
+                   </div>
+                </div>
+
+                <div class="kurs-bottom">
+                    <span>Nästa lektion: Tis 13:30</span>
+                    <span>Klassrum: Sal 11</span>
+                </div>
+
+            </div>
+
+            <div class="kurs kurs-6">
+
+            <div class="kurs-top" style="background-color: #1bd52e;">
+                    <h2>Kursnamn</h2>
+                    <span>Kurslärare</span>
+                </div>
+
+                <div class="kurs-middle">
+                    <div class="circle">
+                   <i class="fa-regular fa-circle-user"></i>
+                   </div>
+                </div>
+
+                <div class="kurs-bottom">
+                    <span>Nästa lektion: Tis 13:30</span>
                     <span>Klassrum: Sal 11</span>
                 </div>
 
@@ -97,24 +172,27 @@
         </div>
         <a class="skapa-kurs" id="myBtn"><i class="fa-solid fa-file-circle-plus"></i> Skapa kurs</a>
 
+        </div>
+
         <div id="myModal" class="modal">
 
             <!-- Modal content -->
             <div class="modal-content">
+                <div class="header-pop">
+                <h2>Skapa ny kurs</h2>
                 <span class="close">&times;</span>
-
+                </div>
                 <input class="kurs-titel" type="text" placeholder="Kurstitel">
-                <p>Välj färg</p>
+                <span class="pick-color-text">Välj färg</span>
                 <div class="color-picker" id="colorPickerContainer">
                     <input class="color-wheel" type="color" value="#6026b8">
                     <i class="fa-solid fa-brush"></i>
                 </div>
-                <br>
                 <a class="members-btn" href="#">Bjud in deltagare</a>
                 <span class="members-amount">&nbsp;(0/100)</span>
-                <div class="create-container">
+     
                     <a class="c-btn" href="#">Skapa kurs</a>
-                </div>
+
             </div>
 
         </div>
