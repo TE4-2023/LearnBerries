0f902a6414a003100715f08f2646dac84014ebcc
Smargi <margo.zarytovska@elev.ntig.se>
2023-11-27 13:44:38 +0100

    can create course messages

--- Includes/insertPosts.php
+++ Includes/insertPosts.php
@@ -5,7 +5,7 @@ require 'connect.php';
 require 'functions.php';
 
 if ($_SESSION['role'] < 3) {
-
+    // Redirect or handle unauthorized access
 }
 
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
@@ -13,27 +13,51 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 
     $courseID = $_POST['course'];
     $name = $_POST['name'];
-    $deadline = $_POST['deadline'];
     $description = $_POST['description'];
     $userID = $_SESSION['userid'];
 
-    try {
-        $query = $pdo->prepare('INSERT INTO posts (course_ID, user_ID, name_ID, publishingDate, deadlineDate, description) 
-                               VALUES (:courseID, :userID, :nameID, NOW(), :deadline, :description)');
-        $data = array(
-            ':courseID' => $courseID,
-            ':userID' => $userID,
-            ':nameID' => $name,  // Adjust this based on your database schema
-            ':deadline' => $deadline,
-            ':description' => $description
-        );
+    // Check if the form is for a post or a message
+    if (isset($_POST['deadlineDate'])) {
+        // It's a post
+        $deadline = $_POST['deadlineDate'];
 
-        $query->execute($data);
+        try {
+            $query = $pdo->prepare('INSERT INTO posts (course_ID, user_ID, name_ID, publishingDate, deadlineDate, description) 
+                                   VALUES (:courseID, :userID, :nameID, NOW(), :deadline, :description)');
+            $data = array(
+                ':courseID' => $courseID,
+                ':userID' => $userID,
+                ':nameID' => checkName($name),  
+                ':deadline' => $deadline,
+                ':description' => $description
+            );
 
-        header('Location: ../kurser.php');
+            $query->execute($data);
 
-    } catch (PDOException $e) {
-        echo 'Error: ' . $e->getMessage();
+            header('Location: ../kurser.php');
+
+        } catch (PDOException $e) {
+            echo 'Error: ' . $e->getMessage();
+        }
+    } else {
+        // It's a message
+        try {
+            $query = $pdo->prepare('INSERT INTO posts (course_ID, user_ID, name_ID, publishingDate, description) 
+                                   VALUES (:courseID, :userID, :nameID, NOW(), :description)');
+            $data = array(
+                ':courseID' => $courseID,
+                ':userID' => $userID,
+                ':nameID' => checkName($name),  
+                ':description' => $description
+            );
+
+            $query->execute($data);
+
+            header('Location: ../kurser.php');
+
+        } catch (PDOException $e) {
+            echo 'Error: ' . $e->getMessage();
+        }
     }
 }
 ?>
