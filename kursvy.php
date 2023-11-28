8ab71a8a45fc86e0c9815cc714525d440cb44c19
emilahnberg123 <emil.ahnberg@elev.ga.ntig.se>
2023-11-28 09:12:13 +0100

    Add kursvy.php and kursvy.css

--- kursvy.php
+++ kursvy.php
@@ -1,6 +1,19 @@
 <?php
 require 'Includes/connect.php';
-include_once "Includes/header.php";
+session_start();
+
+if(!isset($_SESSION['uid'])) { //switch this out
+    //Going back to login page
+    header("location: index.php"); // This will redirect
+    // differently depending on where you use the code, if you include
+    // it in a file thats in a folder it will not find index.php.
+    // This is because the code is still executed from the original
+    // script.
+}
+else {
+    //echo($_SESSION['uid']." ");
+    
+}
 include 'Includes/courseview.php';
 
 /* IMPORTANT NOTICE */
@@ -17,13 +30,43 @@ include 'Includes/courseview.php';
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Kursvy</title>
+    <link rel="stylesheet" href="kursvy.css">
+    <script src="https://kit.fontawesome.com/ef1241843c.js" crossorigin="anonymous"></script>
 </head>
 <body>
-<div style="width:100%;height:30%;display:flex;justify-content:center;background-color:<?php getCourseColor(); ?>;border-bottom-right-radius:1.5vh;border-bottom-left-radius:1.5vh;align-items:center;flex-direction:column;flex-wrap:wrap;">
+<nav>
+            <div class="navbar">
+                <ul>
+                    <li><img class="bild" src="logga.png" alt="logga" /></li>
+                    <li>
+                        <h1 class="header">Kontakter</h1>
+                    </li>
+
+                    <div class="left-nav">
+                        <li><a href=""><i class="fa-solid fa-arrow-right-from-bracket"></i> Logga ut</a></li>
+                    </div>
+                </ul>
+            </div>
+</nav>
+
+<nav>
+            <div class="vert-nav">
+                <ul>
+                    <li><a href=""><i class="fa-solid fa-house"></i> Hem</a></li>
+                    <li><a href=""><i class="fa-solid fa-scroll"></i> Kurser</a></li>
+                    <li><a href=""><i class="fa-regular fa-calendar-days"></i> Scheman</a></li>
+                    <li><a href=""><i class="fa-solid fa-file-pen"></i> Närvaro</a></li>
+                    <li><a href=""><i class="fa-solid fa-newspaper"></i> Nyheter</a></li>
+                    <li><a href=""><i class="fa-solid fa-address-book"></i> Kontakter</a></li>
+                </ul>
+            </div>
+</nav>
+
+<div class="kurs" style="background-color:<?php getCourseColor(); ?>;">
         <h1 style="color:white;text-decoration:none !important;"><?php getCourseName(); ?></h1><br>
         <p style="color:white;text-decoration:none !important;">Lärare A</p>
-    </div>
+</div>
     
     <div class="pane" style="width:100%;height:100%;display:flex;flex-direction:column;flex-wrap:wrap; align-items:center;">
 
@@ -38,7 +81,6 @@ include 'Includes/courseview.php';
 
             while ($row = $query->fetch(PDO::FETCH_ASSOC)) {
               echo '<div style="width: 50%; display:flex; flex-direction:column; flex-wrap:wrap; border-top-left-radius:1vh; border-bottom-right-radius:1vh; height: 10%; margin-top:5%; background-color:white;border:1px solid black;">';
-                echo '<p>Post ID: ' . $row['post_ID'] . '</p>';
                 echo '<p>Deadline: ' . $row['deadlineDate'] . '</p>';
                 echo '<p>Description: ' . $row['description'] . '</p>';
                 // Add more fields as needed
