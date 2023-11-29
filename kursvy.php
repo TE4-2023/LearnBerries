e0ca11149694835c12397de7bcf5ab512d422da8
grod-ai <61289361+grod-ai@users.noreply.github.com>
2023-11-29 10:34:10 +0100

    fixes

--- kursvy.php
+++ kursvy.php
@@ -59,7 +59,7 @@ include 'Includes/courseview.php';
 <div class="kurs" style="background-color:<?php getCourseColor(); ?>;">
         <h1 class="text" style="color:white;text-decoration:none !important;"><?php getCourseName(); ?></h1><br>
         <p class="text" style="color:white;text-decoration:none !important;">Lärare </p>
-        <a href="" class="deltagare" >Deltagare</a>
+        <a href="#" class="deltagare"><i class="fa-solid fa-users"></i> Deltagare</a>
 </div>
 
 
