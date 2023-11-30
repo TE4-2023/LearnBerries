d3b1e3d1da0e25b0d2b038dcefb435c05f7d4683
grod-ai <61289361+grod-ai@users.noreply.github.com>
2023-11-30 10:41:11 +0100

    kursvy klar

--- kursvy.php
+++ kursvy.php
@@ -100,8 +100,10 @@ include 'Includes/courseview.php';
             echo '</div>';
             echo '<div class="uppgift-content">';
             echo '<i class="fa-solid fa-clipboard"></i>';
+            echo '<div class="uppgift-title">';
             echo '<h2>'. $row['name'] . '</h2>';
             echo '<p class="meddelande">' . $row['description'] . '</p>';
+            echo "</div>";
 
 
             echo '</div>';
@@ -111,10 +113,10 @@ include 'Includes/courseview.php';
             echo '<i class="fa-solid fa-clipboard"></i>';
             echo '<h2>' . $row['name'] . '</h2>';
             echo '<p class="meddelande">' . $row['description'] . '</p>';
-            echo '<div class="edits">';
-            echo '<a class="edit-trash" href="#"><i class="fa-regular fa-trash-can"></i></a>';
-            echo '<a class="edit-pen" href="#"><i class="fa-regular fa-pen-to-square"></i></a>';
-            echo '</div>';
+            // echo '<div class="edits">';
+            // echo '<a class="edit-trash" href="#"><i class="fa-regular fa-trash-can"></i></a>';
+            // echo '<a class="edit-pen" href="#"><i class="fa-regular fa-pen-to-square"></i></a>';
+            // echo '</div>';
             echo '</div>';
 
         }
