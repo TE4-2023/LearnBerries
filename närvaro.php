90510c45987c55a6699c3968726c5798c35a94b5
Emil Gothilander <emil.gothilander@elev.ga.ntig.se>
2024-01-12 15:18:08 +0100

    Uppdaterar närvarohantering

--- närvaro.php
+++ närvaro.php
@@ -90,7 +90,6 @@ function getName($userID, $pdo)
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <script src="https://kit.fontawesome.com/ef1241843c.js" crossorigin="anonymous"></script>
-    <!-- <link rel="stylesheet" href="kurser_style.css"> -->
     <link rel="stylesheet" href="närvaro.css">
     <title>LearnBerries</title>
     <script>
